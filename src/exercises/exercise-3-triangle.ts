a237d589a7ae1ca5d9da8d048a49df7d3854feea
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-12 12:35:26 +0100

    Exercise for triangle area calculation

---
+++ src/exercises/exercise-3-triangle.ts
@@ -0,0 +1,6 @@
+export const triangleArea = (
+  triangleBase: number,
+  triangleHeight: number
+): number => {
+  return 0.5 * triangleBase * triangleHeight
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { minutesToSeconds } from './exercises/exercise-1-minutes-to-seconds'
+import { triangleArea } from './exercises/exercise-3-triangle'
 
 console.log(minutesToSeconds(10))
+console.log(triangleArea(3, 5))
