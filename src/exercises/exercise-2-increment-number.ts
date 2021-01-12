742a935ae9da2a0c0ab6a988d7514f6bd1c6fac0
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-12 12:44:29 +0100

    Adding comment to function

--- src/exercises/exercise-2-increment-number.ts
+++ src/exercises/exercise-2-increment-number.ts
@@ -1,3 +1,8 @@
+/**
+ * Function that increments a number by 1
+ * @param startingNumber - The starting number that is to be set
+ * @returns the number incremented by 1
+ */
 export const incrementNumber = (startingNumber: number): number => {
   return startingNumber + 1
 }
