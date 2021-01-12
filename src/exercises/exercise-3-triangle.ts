0f96823e148c6f8675a57d76282184222df0edfd
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-12 13:29:56 +0100

    Correcting formula

--- src/exercises/exercise-3-triangle.ts
+++ src/exercises/exercise-3-triangle.ts
@@ -8,5 +8,5 @@ export const triangleArea = (
   triangleBase: number,
   triangleHeight: number
 ): number => {
-  return 0.5 * triangleBase * triangleHeight
+  return 0.5 * (triangleBase * triangleHeight)
 }
