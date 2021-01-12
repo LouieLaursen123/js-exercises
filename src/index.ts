ac3a3caff574e2b184305cd7c67094226e5e1f31
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-12 15:08:44 +0100

    Updating index

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 //Exercise 1 import
 import { minutesToSeconds } from './exercises/exercise-1-minutes-to-seconds'
-<<<<<<< HEAD
-import { ageToDays } from './exercises/exercise-4-age-to-days'
-=======
 //Exercise 2 import
 import { incrementNumber } from './exercises/exercise-2-increment-number'
-//Exervise 3 import
+//Exercise 3 import
 import { triangleArea } from './exercises/exercise-3-triangle'
->>>>>>> Exercise-3-triangle
+//Exercise 4 import
+import { ageToDays } from './exercises/exercise-4-age-to-days'
 
 //Exervise 1
 console.log(minutesToSeconds(10))
-<<<<<<< HEAD
-console.log(ageToDays(20))
-=======
 //Exercise 2
 console.log(incrementNumber(0))
 //Exercise 3
 console.log(triangleArea(3, 5))
->>>>>>> Exercise-3-triangle
+//Exercise 4
+console.log(ageToDays(20))
