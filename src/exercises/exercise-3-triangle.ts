cb14eeb2e19c85b752a8ab8275baec8e5b503e64
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-12 12:46:39 +0100

    Adding comment to the function

--- src/exercises/exercise-3-triangle.ts
+++ src/exercises/exercise-3-triangle.ts
@@ -1,3 +1,9 @@
+/**
+ * Function that takes the area of a Triangle
+ * @param triangleBase - The width of the bottom of the Triangle
+ * @param triangleHeight - The height of the Triangle
+ * @returns The calculated Area of the triangle
+ */
 export const triangleArea = (
   triangleBase: number,
   triangleHeight: number
