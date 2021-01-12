0ab16d0fec5c34be26426ee4d1f0adab0ee24786
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-12 12:49:31 +0100

    Adding comment to function

--- src/exercises/exercise-4-age-to-days.ts
+++ src/exercises/exercise-4-age-to-days.ts
@@ -1,3 +1,8 @@
+/**
+ * Function that converts ages to days
+ * @param age - The amount of ages
+ * @returns The converted days from ages, by the amount of days there is for one age
+ */
 export const ageToDays = (age: number): number => {
   return age * 365
 }
