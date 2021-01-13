41863ff52d04f4cc0a873c8edbc8a622a633b3b3
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-13 13:35:26 +0100

    Exercise 6 get first value

---
+++ src/exercises/exercise-6-first-element-array.ts
@@ -0,0 +1,12 @@
+/**
+ * Function that converts ages to days
+ * @param age - The amount of ages
+ * @returns The converted days from ages, by the amount of days there is for one age
+ */
+
+const getFirstValue = [90, 50, 7, 100, 150]
+const [firstValue] = getFirstValue
+
+export const getFirstIndex = () => {
+  console.log(firstValue)
+}
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import { triangleArea } from './exercises/exercise-3-triangle'
 import { ageToDays } from './exercises/exercise-4-age-to-days'
 //Exercise 5 import
 import { powerCalculator } from './exercises/exercise-5-power-calculator'
+//Exercise 6 import
+import { getFirstIndex } from './exercises/exercise-6-first-element-array'
 
 //Exervise 1
 console.log(minutesToSeconds(10))
@@ -19,3 +21,5 @@ console.log(triangleArea(3, 5))
 console.log(ageToDays(20))
 //Exercise 5
 console.log(powerCalculator(230, 10))
+//Exercise 6
+getFirstIndex()
