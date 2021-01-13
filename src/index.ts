7a4db911cdb18b365d55a33751995d453339a48a
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-13 15:51:16 +0100

    Exercise 9: less than zero

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ import { incrementNumber } from './exercises/exercise-2-increment-number'
 import { triangleArea } from './exercises/exercise-3-triangle'
 //Exercise 4 import
 import { ageToDays } from './exercises/exercise-4-age-to-days'
-
+//Exercise 9 import
+import { lessThenZero } from './exercises/exercise-9-less-than-zero'
 //Exervise 1
 console.log(minutesToSeconds(10))
 //Exercise 2
@@ -15,3 +16,5 @@ console.log(incrementNumber(0))
 console.log(triangleArea(3, 5))
 //Exercise 4
 console.log(ageToDays(20))
+//Exercise 9
+console.log(lessThenZero(1))
