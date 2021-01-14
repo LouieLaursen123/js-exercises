11a1a5b88c072017efb2f4bbaa61b1ea2c41514d
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-14 14:39:52 +0100

    Correcting comments

--- src/exercises/exercise-10-less-than-hundred.ts
+++ src/exercises/exercise-10-less-than-hundred.ts
@@ -1,7 +1,8 @@
 /**
- * Function that converts minutes to seconds
- * @param minutes - Amount of minutes that is to be converted to seconds
- * @returns minutes input times 60
+ * Function that checks if the sum of the two parameters are less than 100
+ * @param sumOne - The first number that is to be set of the sum
+ * @param sumTwo - The second number that is to be set of the sum
+ * @returns Return true if the sum of the two numbers are less than 100, otherwise return false
  */
 
 export const lessThanHundred = (sumOne: number, sumTwo: number): boolean => {
