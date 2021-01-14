7f512345907d645008e70e682f3741e82751ce3b
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-14 14:49:15 +0100

    Exercise-10-operator-boolean

---
+++ src/exercises/Exercise-11-operator.ts
@@ -0,0 +1,13 @@
+/**
+ * Function that converts minutes to seconds
+ * @param minutes - Amount of minutes that is to be converted to seconds
+ * @returns minutes input times 60
+ */
+
+export const tsOperator = (valueOne: boolean, valueTwo: boolean): boolean => {
+  if (valueOne && valueTwo) {
+    return true
+  } else {
+    return false
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { incrementNumber } from './exercises/exercise-2-increment-number'
 import { triangleArea } from './exercises/exercise-3-triangle'
 //Exercise 4 import
 import { ageToDays } from './exercises/exercise-4-age-to-days'
+//Exercise 11 import
+import { tsOperator } from './exercises/Exercise-11-operator'
 
 //Exervise 1
 console.log(minutesToSeconds(10))
@@ -15,3 +17,5 @@ console.log(incrementNumber(0))
 console.log(triangleArea(3, 5))
 //Exercise 4
 console.log(ageToDays(20))
+//Exercise 11
+console.log(tsOperator(true, true))
