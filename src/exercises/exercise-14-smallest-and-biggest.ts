eb95115d0bed7c60e4b41b8692b8c8bbde510c69
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-15 12:58:45 +0100

    Exercise 14 minmax number

---
+++ src/exercises/exercise-14-smallest-and-biggest.ts
@@ -0,0 +1,10 @@
+/**
+ * Function that adds up numbers from 1
+ * @returns The overall sum of the value from startNum from number 1
+ */
+
+const numbersList = [1, 3, 7, 11, 21]
+
+export const minMax = (): number => {
+  return Math.min(...numbersList) && Math.max(...numbersList)
+}
