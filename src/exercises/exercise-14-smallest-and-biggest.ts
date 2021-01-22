03b9eae6c222f648231984cfff8cc6dac8b243ca
Louie Bay Laursen <louie.bay@adaptagency.com>
2021-01-22 13:58:02 +0100

    Correcting comments

--- src/exercises/exercise-14-smallest-and-biggest.ts
+++ src/exercises/exercise-14-smallest-and-biggest.ts
@@ -1,6 +1,6 @@
 /**
- * Function that adds up numbers from 1
- * @returns The overall sum of the value from startNum from number 1
+ * Function that finds the minimum number and the maximum number
+ * @returns A new array with only the minimum number and the maximum number
  */
 
 const numbersList = [1, 3, 7, 11, 21]
